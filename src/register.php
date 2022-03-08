2c028d4e50227ae8d3fa3ac85bbcadc67ec2477b
Josh-Cox <joshcox2@hotmail.com>
2022-03-08 15:33:33 +0000

    Finished register form

--- src/register.php
+++ src/register.php
@@ -21,6 +21,12 @@
                     <input type="text" name="uName" placeholder="Username">
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="cPwd" placeholder="Confirm Password">
+                    <input type="radio" id=radioYes name="display" value="yes" checked>
+                    <input type="radio" id="radioNo" name="display" value="no">
+                    <div class="switch">
+                        <label for="radioYes" chec>Yes</label>
+                        <label for="radioNo" chec>No</label>
+                    </div>
                     <button type="submit" name="submit">Sign Up</button>
                 </form>
             </div>
